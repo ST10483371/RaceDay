8302e52304506b34f37df39ee246bf38f67f967e
ST10483371 <st10483371@rcconnect.edu.za>
2026-09-04 22:28:40 +0200

    Created Participants table
    Added columns participantid, dateofbirth, gender, emergency_contact, team_name to Participants table.

--- docs/RaceDay.sql
+++ docs/RaceDay.sql
@@ -10,3 +10,11 @@ ContactInfo VARCHAR(10),
 UserType VARCHAR(50) NOT NULL,
 CHECK (UserType IN ('Participant', 'Organizer'))
 )
+
+CREATE TABLE Participants(
+ParticipantId int IDENTITY(1,1) PRIMARY KEY,
+DateOfBirth VARCHAR(20) NOT NULL,
+Gender VARCHAR(20) NOT NULL,
+EmergencyContact VARCHAR(20) 
+TeamName VARCHAR(20)
+)
