271f6e7d95f82ce358881f3f8f9f938da28f1e31
ST10483371 <st10483371@rcconnect.edu.za>
2026-09-04 22:18:55 +0200

    Define RaceDay database and Users table schema
    creating users table with variables name, email, password, contact, UserType

--- docs/RaceDay.sql
+++ docs/RaceDay.sql
@@ -1,2 +1,12 @@
 Create database RaceDay;
 Use RaceDay;
+
+CREATE TABLE Users(
+UserID int IDENTITY(1,1) PRIMARY KEY,
+Name VARCHAR(50) NOT NULL,
+Email VARCHAR(20) UNIQUE NOT NULL,
+Password VARCHAR(20) NOT NULL,
+ContactInfo VARCHAR(10),
+UserType VARCHAR(50) NOT NULL,
+CHECK (UserType IN ('Participant', 'Organizer'))
+)
